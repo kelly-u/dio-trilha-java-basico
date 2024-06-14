a9a408483fb2794b8d30dc52ec23c6de7e3a78d5
G. Kelly Macedo <gleicekellymacedodd@gmail.com>
2024-06-14 19:15:49 -0300

    Finalizei o conteúdo de argumentos pelo terminal

---
+++ tipo-variaveis/src/edu/kelly/bootcampdio/terminal/MinhaClasse.java
@@ -0,0 +1,9 @@
+package edu.kelly.bootcampdio.terminal;
+
+public class MinhaClasse {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Oi turma, fui executado pelo terminal");
+    
+        
+    }
+}
