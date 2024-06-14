94cc78a341ea987ffa756c30bfbba439e81f7bb5
G. Kelly Macedo <gleicekellymacedodd@gmail.com>
2024-06-14 17:28:03 -0300

    Criacao da classe calculadora, Comentarios e explicacao da geracao de uma api web

---
+++ tipo-variaveis/src/edu/kelly/bootcampdio/documentacao/Calculadora.java
@@ -0,0 +1,27 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros.
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @autor G. Kelly Macedo
+ * @version 1.0
+ * @since 14/06/2024
+ */
+
+package edu.kelly.bootcampdio.documentacao;
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmentro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números
+     */
+
+    public int somar (int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+
+}
---
+++ tipo-variaveis/src/edu/kelly/bootcampdio/documentacao/Comentarios.java
@@ -0,0 +1,26 @@
+package edu.kelly.bootcampdio.documentacao;
+
+public class Comentarios {
+    
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+        * eu sou um comentário
+        * que posso ser mais detalhado
+        * quando necessário
+        */         
+
+    }
+
+      /**
+         * Estaas duas estrelinhas acima
+         * são para identificar que você
+         * pretende elaborar um comentário
+         * a nível de documentação.
+         * Que incrível !!!
+      */
+    public void metodo(){
+            
+    }
+}
