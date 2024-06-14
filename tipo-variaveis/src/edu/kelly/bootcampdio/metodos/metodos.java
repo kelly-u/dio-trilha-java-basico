d33c49980f8fe5bb292263eba35711936810cbb9
G. Kelly Macedo <gleicekellymacedodd@gmail.com>
2024-06-14 14:01:35 -0300

    Adicionando a classe metodos

---
+++ tipo-variaveis/src/edu/kelly/bootcampdio/metodos/metodos.java
@@ -0,0 +1,13 @@
+package edu.kelly.bootcampdio.metodos;
+
+public class metodos {
+
+// metododos devem ser verbos
+// seguir o camelCase
+// não existe método global, todos devem ser escritos dentro de uma classe.
+
+    public static void main(String[] args) {
+        
+    }
+
+}
