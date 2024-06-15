6338b8db2d90b7a835d424194b6c644c7f38df3e
G. Kelly Macedo <gleicekellymacedodd@gmail.com>
2024-06-15 17:11:15 -0300

    Finalização da classe ResultadoEscolar2

--- src/edu/kelly/bootcampdio/estruturascondicionais/ResultadoEscolar2.java
+++ src/edu/kelly/bootcampdio/estruturascondicionais/ResultadoEscolar2.java
@@ -2,11 +2,14 @@ package edu.kelly.bootcampdio.estruturascondicionais;
 
 public class ResultadoEscolar2 {
     public static void main(String[] args) {
-        int nota  = 8;
+        int nota  = 10;
 
-    // Condicional composta - tem dois caminhos
+    // Condicional encadeada - tem vários caminhos caminhos
     if(nota >= 7){
         System.out.println("Aprovado");
+    }
+    else if (nota >= 5 && nota < 7){
+        System.out.println("Prova de Recuperação");
     }
 
     else{
