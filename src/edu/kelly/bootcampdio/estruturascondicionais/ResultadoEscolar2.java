5d7aa38e9073fe55c66bc1465620c2d56580a4f5
G. Kelly Macedo <gleicekellymacedodd@gmail.com>
2024-06-15 17:05:18 -0300

    Criação da classe ResultadoEscolar 2

---
+++ src/edu/kelly/bootcampdio/estruturascondicionais/ResultadoEscolar2.java
@@ -0,0 +1,16 @@
+package edu.kelly.bootcampdio.estruturascondicionais;
+
+public class ResultadoEscolar2 {
+    public static void main(String[] args) {
+        int nota  = 8;
+
+    // Condicional composta - tem dois caminhos
+    if(nota >= 7){
+        System.out.println("Aprovado");
+    }
+
+    else{
+        System.out.println("Reprovado");
+    }
+    }
+}
