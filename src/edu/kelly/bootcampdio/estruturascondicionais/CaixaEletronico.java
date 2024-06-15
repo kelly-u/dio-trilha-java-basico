ce07edf513c95e6ecada2e9799152bb6f6aec778
G. Kelly Macedo <gleicekellymacedodd@gmail.com>
2024-06-15 16:47:04 -0300

    finalizando a classe CaixaEletronico

--- src/edu/kelly/bootcampdio/estruturascondicionais/CaixaEletronico.java
+++ src/edu/kelly/bootcampdio/estruturascondicionais/CaixaEletronico.java
@@ -1,5 +1,15 @@
 package edu.kelly.bootcampdio.estruturascondicionais;
 
 public class CaixaEletronico {
-    
+    public static void main(String[] args) {
+       double saldo = 25.0;
+       double valorSolicitado = 17.0;
+
+       // Condicional simples - só tem um caminho (o caminho feliz)
+       if (valorSolicitado < saldo){
+        saldo -= valorSolicitado;
+       }
+
+       System.out.println(saldo);
+    }
 }
