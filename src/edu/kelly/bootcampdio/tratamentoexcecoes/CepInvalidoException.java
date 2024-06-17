d28172317877809811ee846f46135075dccb3a80
G. Kelly Macedo <gleicekellymacedodd@gmail.com>
2024-06-17 09:36:44 -0300

    Criação e implementação de uma exception

---
+++ src/edu/kelly/bootcampdio/tratamentoexcecoes/CepInvalidoException.java
@@ -0,0 +1,6 @@
+package edu.kelly.bootcampdio.tratamentoexcecoes;
+
+// Criação de uma exception personalizada
+public class CepInvalidoException extends Exception {
+    
+}
---
+++ src/edu/kelly/bootcampdio/tratamentoexcecoes/FormatadorCep.java
@@ -0,0 +1,23 @@
+package edu.kelly.bootcampdio.tratamentoexcecoes;
+
+public class FormatadorCep {
+    public static void main(String[] args) {
+        try{
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        }
+        catch (CepInvalidoException e){
+            System.out.println("O cep não corresponde às regras de negócio.");
+        }
+    }
+
+    // Uso da excessõa criada na classe CepInvalidoException
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if (cep.length() != 8)
+            throw new CepInvalidoException();
+
+        // simulando um cep formatado
+        return "23.765-064";
+    }
+
+}
